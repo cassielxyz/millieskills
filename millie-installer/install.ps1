1145a93a3e2b8a29e2622be5ddc030ad7bf52b51
M.A Jesowin Raja <soulcassiel@gmail.com>
2026-08-26 11:00:02 +0530

    Update default repository in install.ps1

--- millie-installer/install.ps1
+++ millie-installer/install.ps1
@@ -1,5 +1,5 @@
 param(
-    [string]$Repository = "YOUR_GITHUB_USERNAME/YOUR_REPOSITORY",
+    [string]$Repository = "cassielxyz/millieskills",
     [string]$Branch = "main",
     [string]$Skill,
     [string]$Platform,
